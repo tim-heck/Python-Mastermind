3631f34117277f2daaa3e8282da76d77bb6e659a
tih99924 <tih99924@bethel.edu>
2016-02-25 14:16:16 -0600

    Added while loop for reprompt

--- Mastermind.py
+++ Mastermind.py
@@ -2,6 +2,11 @@
 # Mastermind python mini project
 # Tim Heck
 # 02/23/16
+# User enters a four digit number to try to guess the
+# randomly generated four digit number, cows are
+# counted if a digit correctly matches the specific place
+# and a bull if a digit correctly matches but is not in
+# the correct place
 #################################
 
 from random import randint
@@ -24,14 +29,21 @@ def error_check(n):
     if len(n) < 4 or len(n) > 4:
         print "You did not enter a four digit number"
         return False
-    #Check if not number?
+    # Check if not number?
     return True
 
 
-def check_guess(num, rand_int):
-    new_num = ""
+# Method for comparing the user guess with the randomly
+# generated number. Counts the number of cows and the number
+# of bulls
+def compare_guess(num, rand_int):
+    new_num = ""  # The new number with removed cows
     cows = 0
     count1 = 0
+    # Checks each digit from user enter number with the
+    # randomly generated number for correct position
+    # counts the cows, if a cow is found, that digit from
+    # the user entered number is removed
     for i in num:
         if str(i) == str(rand_int[count1]):
             new_num += ""
@@ -39,39 +51,40 @@ def check_guess(num, rand_int):
         else:
             new_num = new_num + rand_int[count1]
         count1 += 1
-    print cows
-    print new_num
 
     bulls = 0
+    # Checks each digit of the new number with each digit
+    # in the randomly generated number to count bulls
     for i in new_num:
         for j in num:
             if str(i) == str(j):
                 bulls += 1
-    print bulls
 
-    return str(cows) + " cow(s) " + str(bulls) + " bull(s)"
-
-
-# def game_won(num_cows):
-#     if num_cows == 4:
-#         return True
+    return str(cows) + " cow(s), " + str(bulls) + " bull(s)"
 
 
 ####################################################
 
 
+print ""
 print "Welcome to the Cows and Bulls Game!"
 print ""
-print "Guess the random four digit number by receiving a cow\n" \
-      "for every digit that you guess correctly in the correct\n" \
-      "place and a bull if it is not a cow you guess correctly,\n" \
-      "but in the wrong place. Good luck!"
+print "Guess the randomly generated four digit number by receiving a\n" \
+      "cow for every digit that you guess correctly in the correct\n" \
+      "place, and a bull if it is not a cow you and if you guess\n" \
+      "correctly, but in the wrong place. Good luck!"
 print ""
 
-# while (game_won)
-user_num = raw_input("Enter a four digit number: ")
+user_num = 1
 
-while error_check(user_num) == 0:
+# Executes until the user enter number correctly matches
+# the randomly generated number
+while rand_number != user_num:
     user_num = raw_input("Enter a four digit number: ")
 
-print check_guess(user_num, rand_number)
+    while error_check(user_num) == 0:
+        user_num = raw_input("Enter a four digit number: ")
+
+    print compare_guess(user_num, rand_number)
+
+print "You win!"
