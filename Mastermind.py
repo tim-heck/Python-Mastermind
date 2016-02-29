511abe40bbfa23fb3bf5decd404737abe69bc543
tih99924 <tih99924@bethel.edu>
2016-02-29 14:23:47 -0600

    Changes cow to ‘E’ and replaces bull with ‘E’

--- Mastermind.py
+++ Mastermind.py
@@ -38,6 +38,7 @@ def error_check(n):
 # of bulls
 def compare_guess(num, rand_int):
     new_num = ""  # The new number with removed cows
+    # new_num = []
     cows = 0
     count1 = 0
     # Checks each digit from user enter number with the
@@ -46,22 +47,28 @@ def compare_guess(num, rand_int):
     # the user entered number is removed
     for i in num:
         if str(i) == str(rand_int[count1]):
-            new_num += ""
+            new_num += "E"
+            # new_num.append("E")
             cows += 1
         else:
-            new_num = new_num + rand_int[count1]
+            new_num += rand_int[count1]
+            # new_num.append(num[count1])
         count1 += 1
+
+    print new_num
 
     bulls = 0
     # Checks each digit of the new number with each digit
     # in the randomly generated number to count bulls
-    for i in num:
-        for j in new_num:
-            if str(i) == str(j):
+    for i in range(0, len(new_num)):
+        for j in range(0, 4):
+            if str(new_num[i]) == str(num[j]):
                 bulls += 1
-                # Eliminates double counting for bulls
-                if bulls == (bulls - 1) + 1:
-                    break
+                new_num = new_num[:i] + "E" + new_num[i+1:]
+                # Eliminates double counting for bulls for multiple numbers
+                # if bulls == (bulls - 1) + 1:
+                break
+    print new_num
 
     return str(cows) + " cow(s), " + str(bulls) + " bull(s)"
 
@@ -79,6 +86,7 @@ print "Guess the randomly generated four digit number by receiving a\n" \
 print ""
 
 user_num = 1
+num_guesses = 0
 
 # Executes until the user enter number correctly matches
 # the randomly generated number
@@ -89,5 +97,6 @@ while rand_number != user_num:
         user_num = raw_input("Enter a four digit number: ")
 
     print compare_guess(user_num, rand_number)
+    num_guesses += 1
 
-print "You win!"
+print "You win! It took you " + str(num_guesses) + " guess(es)."
